d2cb3db63f966447a7eb88d64998acbb2050dcde
Zane Matero <materozane@gmail.com>
2023-03-12 14:08:35 -0700

    gallery functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import Gallery from './Gallery'
+import ButtonBar from './ButtonBar'
 
-function App() {
+
+function App(){
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+  useEffect(() => {
+      document.title='Welcome to Artworld'
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Gallery objectImage={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+      <ButtonBar handleIterate={handleIterate}/>
       </header>
     </div>
   );
